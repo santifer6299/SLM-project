eae8af4bb774ea2e2b021449731af539ab0a5688
alminhuremovic <almin.huremovic@outlook.com>
2024-01-10 19:51:39 +0100

    added jar file

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.tictactoe {
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires org.kordamp.bootstrapfx.core;
+    requires junit;
 
     opens com.example.tictactoe to javafx.fxml;
     exports com.example.tictactoe;
